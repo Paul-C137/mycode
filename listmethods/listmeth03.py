aa57c87153cd65569ef4528a7c802acd11915e92
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2021-04-05 13:34:31 -0700

    Added a demo of the list.insert method

--- listmethods/listmeth03.py
+++ listmethods/listmeth03.py
@@ -10,3 +10,8 @@ proto.extend(proto2) # pass proto2 as an argument to the extend method
 print(proto)
 protoa.append(proto2) # pass proto2 as an argument to the append method
 print(protoa)
+
+# demo of list.insert() method
+proto.insert(0, "paul")
+
+print(proto)
