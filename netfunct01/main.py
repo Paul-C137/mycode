c284318f961ce27cce3426913eefab85808e8170
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2021-04-07 12:22:52 -0700

    Python and physical interfaces

--- netfunct01/main.py
+++ netfunct01/main.py
@@ -10,24 +10,23 @@ def commandpush(devicecmd): # devicecmd==list
             print(crayons.red('Attempting to sending command --> ' + mycmds ))
             # we'll learn to write code that sends cmds to device here
 
-def devicereboot(x):
-    for ip in x:
-        print(f"\nConnecting to {ip}.")
-    print("REBOOTING NOW!")    
-
+def devicereboot(ip_list):
+    for ip_address in ip_list:
+        print(f"\nConnecting to {ip_list}.")
+    print("REBOOTING NOW!")
 
 # start our main script
 def main():
     work2do = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1":
-    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]} 
+    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]}
     # data that replaces data stored in file
 
     ip_list = ["10.0.0.1", "10.0.0.2", "10.0.0.3"]
 
     print(crayons.blue("Welcome to the network device command pusher")) # welcome message
 
-    ## get data set
-    print(crayons.blue("\nData set found\n")) # replace with function call that reads in data from file
+    # replace with function call that reads in data from file
+    print(crayons.blue("\nData set found\n"))
 
     ## run
     commandpush(work2do) # call function to push commands to devices
@@ -36,3 +35,4 @@ def main():
 
 # call our main function
 main()
+print('\n')
