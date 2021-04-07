d3c8333505176d787394fa83e84d26b0a69f016c
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2021-04-07 14:42:24 -0700

    Learning to work with the matplotlib library

---
+++ lrnimport/intscript.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from subprocess import call
+call(["ip", "link", "show"])
+print("This program will check your interfaces.")
+interface = input("Enter an interface, like, en0: ")
+call(["ip", "addr", "show", "dev", interface])
+call(["ip", "route", "show", "dev", interface])
